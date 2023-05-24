68371d16822083b64b9d2453fdd4819ef55d91e3
Daffa Maulana Haekal <maulanadaffa342@gmail.com>
2023-05-24 21:05:21 +0700

    initial view enrolled

--- enrolled_event/urls.py
+++ enrolled_event/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from enrolled_event.views import indexEnrolledEventAtlet
+from enrolled_event.views import indexEnrolledPartaiKompetisi
 
 app_name = 'enrolled_event'
 
 urlpatterns = [
     path('enrolled_event_atlet/', indexEnrolledEventAtlet, name='index'),
+    path('enrolled_partai_kompetisi/', indexEnrolledPartaiKompetisi, name='index'),
+    
 ]
--- enrolled_event/views.py
+++ enrolled_event/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from Util.query import *
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+import random
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 
 def indexEnrolledEventAtlet(request):
-    return render(request, 'enrolled_event_atlet.html')
+    cursor.execute('set search_path to babadu')
+    email = request.COOKIES.get('email')
+
+    cursor.execute(
+        f"""
+        SELECT pk.nama_event, pk.tahun_event, e.nama_stadium, ppk.jenis_partai, e.kategori_superseries, e.tgl_mulai, e.tgl_selesai
+    FROM PARTAI_KOMPETISI pk
+    JOIN PARTAI_PESERTA_KOMPETISI ppk ON pk.nama_event = ppk.nama_event AND pk.tahun_event = ppk.tahun_event AND pk.jenis_partai = ppk.jenis_partai
+    JOIN EVENT e ON pk.nama_event = e.nama_event AND pk.tahun_event = e.tahun
+    WHERE ppk.nomor_peserta = {email};"""
+    )
+
+    record = cursor.fetchall()[0]
+    nama_event = record[0]
+    tahun_event = record[1]
+    nama_stadium = record[2]
+    jenis_partai = record[3]
+    kategori_superseries = record[4]
+    tgl_mulai = record[5]
+    tgl_selesai = record[6]
+
+    context = {
+        'nama_event': nama_event,
+        'tahun_event': tahun_event,
+        'nama_stadium': nama_stadium,
+        'jenis_partai': jenis_partai,
+        'kategori_superseries' : kategori_superseries ,
+        'tgl_mulai' : tgl_mulai,
+        'tgl_selesai' : tgl_selesai
+
+    }
+
+    return render(request, 'enrolled_event_atlet.html', context)
+
+
+def indexEnrolledPartaiKompetisi(request):
+
+    return render(request, 'enrolled_partai_kompetisi.html')
+
+
+
