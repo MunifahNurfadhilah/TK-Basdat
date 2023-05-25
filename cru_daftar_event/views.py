3b68eae60ea7dfd67153797c9d48f107cb881624
Ahmad Rafi Wirana <rafi.wirana@rey.id>
2023-05-25 15:07:57 +0700

    feat: add cru event

--- cru_daftar_event/views.py
+++ cru_daftar_event/views.py
@@ -2,11 +2,63 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def indexListEvent(request):
-    return render(request, 'list_event.html')
+def list_all_events(request):
+    context = {}
+    db_connection = psycopg2.connect(
+        host="localhost",
+        database="munifah.nurfadhilah",
+        user="postgres",
+        port="5433",
+        password="99tugas"
+    )
+    cursor = db_connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO BABADU;")
+    query = sql_get_all_events()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    list_events = []
+    for event in data:
+        list_events.append(event)
+    context['data_list_events'] = list_events
+    return render(request, "list_events.html", context)
 
-def indexListEventStadium(request):
-    return render(request, 'list_event_stadium.html')
+def list_all_event_stadiums(request):
+    context = {}
+    db_connection = psycopg2.connect(
+        host="localhost",
+        database="munifah.nurfadhilah",
+        user="postgres",
+        port="5433",
+        password="99tugas"
+    )
+    cursor = db_connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO BABADU;")
+    query = sql_get_all_event_stadiums()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    list_event_stadiums = []
+    for event_stadium in data:
+        list_event_stadiums.append(event_stadium)
+    context['data_list_event_stadiums'] = list_event_stadiums
+    return render(request, "list_event_stadiums.html", context)
 
-def indexListEventKategori(request):
-    return render(request, 'list_event_kategori.html')
+
+def list_all_event_categories(request):
+    context = {}
+    db_connection = psycopg2.connect(
+        host="localhost",
+        database="munifah.nurfadhilah",
+        user="postgres",
+        port="5433",
+        password="99tugas"
+    )
+    cursor = db_connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO BABADU;")
+    query = sql_get_all_event_categories()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    list_event_categories = []
+    for event_category in data:
+        list_event_categories.append(event_category)
+    context['data_list_event_categories'] = list_event_categories
+    return render(request, "list_event_categories.html", context)
